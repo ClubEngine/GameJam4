6a58422f76715d39e3aee079128a4e868ea881ab
Adrien Hamraoui <dev@hamadr.fr>
2012-10-21 22:22:05 +0100

    death animation

--- dev/scene/Player.py
+++ dev/scene/Player.py
@@ -32,7 +32,7 @@ class Player:
         self._attackFrameNumber = 0
         self._jumpFrameNumber = 0
         self._deathFrameNumber = 0
-        
+        self._deathTime = 0
     def name(self):
         return self._name
 
@@ -144,7 +144,8 @@ class Player:
            self._jumpTime = 0
 
     def _updateDeath(self, elapsedTime):
-        return
+        self._deathFrameNumber =  min(self._deathFrameNumber+1, 30)
+        
     def jumpRatio(self):
         return self._jumpTime / maxJumpTime
 
