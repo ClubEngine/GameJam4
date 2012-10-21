8085ef314b11ff6eca83e627c435a1f590377394
Dominique Lee <Dominique.Lee@ensimag.imag.fr>
2012-10-21 20:26:13 +0200

    Correction emplacement noms HUD

--- dev/screen/HUD.py
+++ dev/screen/HUD.py
@@ -12,14 +12,14 @@ class HUD:
     def draw(self):
         textPlayer1 = self._font.render(self._player1.name(), True, (0, 0, 0))
 
-        self._screen._window.blit(textPlayer1, [10,10])
+        self._screen._window.blit(textPlayer1, [10, 8])
         pygame.draw.rect(self._screen._window, self.cyan, 
             [10, 33, self._player1._life*2, 10], 0)
         pygame.draw.rect(self._screen._window, self.gray,
             [10,33,200,10], 1)
 
         textPlayer2 = self._font.render(self._player2.name(), True, (0, 0, 0))
-        self._screen._window.blit(textPlayer2, [910, 10])
+        self._screen._window.blit(textPlayer2, [1000 - 10 * (len(self._player2.name()) + 5), 8])
         pygame.draw.rect(self._screen._window, self.cyan, 
             [790+(200-self._player2._life*2), 33, self._player2._life*2, 10], 0)
         pygame.draw.rect(self._screen._window, self.gray,
