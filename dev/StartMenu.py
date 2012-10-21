4dce800e8ec727350f3cfcf221bcb2acb75b3082
benoit person <benoit.person@gmail.com>
2012-10-21 19:56:26 +0200

    start menu with quit button

--- dev/StartMenu.py
+++ dev/StartMenu.py
@@ -1,5 +1,5 @@
 from GameState import GameState
-import pygame
+import pygame, sys
 
 class Colors:
     BLACK       = (   0,   0,   0)
@@ -28,11 +28,21 @@ class StartMenu:
             150,
             30
         )
+        self._endButtonRect = pygame.Rect(
+            self._main._window.get_width()/2-75,
+            100,
+            150,
+            30
+        )
         self._backImg = pygame.image.load("assets/arena/startMenu.png")
 
         self._startButtonListener = self._main._listener.addMouseEvent(
             self._startButtonRect,
             self.startGame
+        )
+        self._endButtonListener = self._main._listener.addMouseEvent(
+            self._endButtonRect,
+            self.quitGame
         )
 
     def drawLoading(self):
@@ -58,9 +68,17 @@ class StartMenu:
             self._startButtonRect,
             0
         )
+        startText = self._font.render("Start game", True, Colors.BLACK)
+        self._main._window.blit(startText, [self._main._window.get_width()/2-54,35])
 
-        text = self._font.render("Start game", True, Colors.BLACK)
-        self._main._window.blit(text, [self._main._window.get_width()/2-54,35])
+        pygame.draw.rect(
+            self._main._window,
+            Colors.LIGHT_GREY,
+            self._endButtonRect,
+            0
+        )
+        quitText = self._font.render("Quit game", True, Colors.BLACK)
+        self._main._window.blit(quitText, [self._main._window.get_width()/2-54,105])
 
         pygame.display.flip()
 
@@ -71,3 +89,12 @@ class StartMenu:
         """
         self._main.state = GameState.IN_GAME
         self._main._listener.removeMouseEvent(self._startButtonListener)
+
+    def quitGame(self):
+        """
+        handler for the click on the "end game" button.
+        @param  self [StartMenu]    itself
+        """
+        self._main.loop = False
+        pygame.quit()
+        sys.exit()
