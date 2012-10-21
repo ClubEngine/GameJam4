3f30bb7c0a6accccc06c97198560ff414c26ded9
benoit person <benoit.person@gmail.com>
2012-10-21 13:21:29 +0200

    premier sprite à l'écran

--- dev/main.py
+++ dev/main.py
@@ -7,14 +7,14 @@ from screen import Screen
 from event import EventListener
 from scene import Scene
 
-WINDOW_WIDTH=640
+WINDOW_WIDTH=1000
 WINDOW_HEIGHT=480
 
 pygame.init()
 
 fpsClock = pygame.time.Clock()
 
-windowSurfaceObj = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+windowSurfaceObj = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.DOUBLEBUF)
 pygame.display.set_caption("Street Pirates Vs. Ninja")
 fontObj = pygame.font.Font('freesansbold.ttf', 42)
 msg = "Hello world!"
--- dev/screen/PlayerSprite.py
+++ dev/screen/PlayerSprite.py
@@ -9,12 +9,12 @@ class PlayerSprite(pygame.sprite.Sprite):
     def __init__(self, imageName, x = 0, y = 0, angle = 0):
         pygame.sprite.Sprite.__init__(self)
         self._spritesList = []     
-        for i in range(36):
-            sprite = pygame.image.load("assets/" + imageName + "-pos-" + i + ".png")
+        for i in range(0, 360, 90):
+            sprite = pygame.image.load("assets/img/" + imageName + "-pos-" + str(i) + ".png")
             self._spritesList.append(sprite)
-        update(x, y, angle)
+        self.update(x, y, angle)
         
-    def update(x, y, angle):
+    def update(self, x, y, angle):
         imageNb = angle / 10;
         self.image = self._spritesList[imageNb]
         self.rect = self.image.get_rect()
