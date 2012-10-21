9f558af8195dcf1fde8ccf55a58996dd23c3c6b2
Dominique Lee <Dominique.Lee@ensimag.imag.fr>
2012-10-21 14:58:24 +0200

    Scene : constant movement

--- dev/scene/Scene.py
+++ dev/scene/Scene.py
@@ -2,6 +2,8 @@ from Player import Player
 from Collision import Collision
 
 distDelta = 0.1
+maxSpeed = 1
+accelerationTime = 1
 
 class Scene:
 
@@ -10,23 +12,24 @@ class Scene:
     def __init__(self):
         self._players = [ Player("player1"), Player("player2") ] 
         self._collision = Collision(self._players[0], self._players[1])
+        # self._inertiasX = [ 0, 0 ]
+        # self._inertiasY = [ 0, 0 ]
 
     def update(self):
         for player in self._players:
             player.update()
 
-    def moveForward(self, playerIndex):
-        self._collision.moveForward(playerIndex, distDelta) 
+    def moveForward(self, playerIndex, timeElapsed):
+        self._collision.moveForward(playerIndex, timeElapsed * maxSpeed) 
 
+    def moveBackward(self, playerIndex, timeElapsed):
+        self._collision.moveForward(playerIndex, -timeElapsed * maxSpeed) 
 
-    def moveBackward(self, playerIndex):
-        self._collision.moveForward(playerIndex, -distDelta) 
+    def moveRight(self, playerIndex, timeElapsed):
+        self._collision.moveSide(playerIndex, timeElapsed * maxSpeed) 
 
-    def moveRight(self, playerIndex):
-        self._collision.moveSide(playerIndex, distDelta) 
-
-    def moveLeft(self, playerIndex):
-        self._collision.moveSide(playerIndex, -distDelta) 
+    def moveLeft(self, playerIndex, timeElapsed):
+        self._collision.moveSide(playerIndex, -timeElapsed * maxSpeed) 
 
 
     def jump(self, playerIndex):
