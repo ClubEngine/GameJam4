391683a53f2887d72e4adc2359c92e95d63ff005
benoit person <benoit.person@gmail.com>
2012-10-21 12:34:06 +0200

    on ajoute de la beauté

--- dev/screen/Grid.py
+++ dev/screen/Grid.py
@@ -1,14 +1,27 @@
 import pygame
 
 class Grid:
+    colors = [
+        ( 255,   0,   0),
+        (   0, 255,   0),
+        (   0,   0, 255)
+    ]
+
     def __init__(self, window):
         self._window = window
+        self._mat = [
+            []
+        ]
+        self._iColor = 0
 
     def draw(self):
         for xOffset in xrange(0, self._window.get_width(), 10):
-            pygame.draw.line(self._window, pygame.Color(255, 0, 0), 
+            pygame.draw.line(self._window, self.colors[self._iColor], 
                 [xOffset, 0], [xOffset, self._window.get_height()], 1)
 
         for yOffset in xrange(0, self._window.get_height(), 10):
-            pygame.draw.line(self._window, pygame.Color(255, 0, 0),
+            pygame.draw.line(self._window, self.colors[self._iColor],
                 [0, yOffset], [self._window.get_width(), yOffset], 1)
+
+        self._iColor+=1
+        self._iColor%=3
