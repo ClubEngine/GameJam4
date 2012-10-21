c9810228a13fefe3cebc52ca2669317b552f2c62
dureuill <louis.dureuil@ensimag.imag.fr>
2012-10-21 14:23:29 +0200

    Ajout des moves dans les 4 "directions"

--- dev/scene/Scene.py
+++ dev/scene/Scene.py
@@ -1,23 +1,32 @@
 from Player import Player
 
+distDelta = 0.1
 
 class Scene:
 
+    
+
     def __init__(self):
-        self._players = [ Player("player1") ] 
+        self._players = [ Player("player1"), Player("player2") ] 
+        self._collision = Collision(players[0], players[1])
 
     def update(self):
         for player in self._players:
             player.update()
 
     def moveForward(self, playerIndex):
-        pass
+        self._collision.moveForward(playerIndex, distDelta) 
+
+
     def moveBackward(self, playerIndex):
-        pass
+        self._collision.moveForward(playerIndex, -distDelta) 
+
     def moveRight(self, playerIndex):
-        pass
+        self._collision.moveSide(playerIndex, distDelta) 
+
     def moveLeft(self, playerIndex):
-        pass
+        self._collision.moveSide(playerIndex, -distDelta) 
+
 
     def jump(self, playerIndex):
         self._players[playerIndex].jump()
