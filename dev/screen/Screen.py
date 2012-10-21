069cd0a06e7ec1315a474fef8728446cb3cb0111
Gilles <gilles@LinBox.(none)>
2012-10-21 15:44:54 +0200

    GJ4:Screen: link with model

--- dev/screen/Screen.py
+++ dev/screen/Screen.py
@@ -15,13 +15,30 @@ class Screen:
         self._grid = Grid(self)
         self._players = pygame.sprite.RenderPlain()
         self._players.add(PlayerSprite('pirate', self))
-        self._players.add(PlayerSprite('pirate', self, 100, 100))
+        self._players.add(PlayerSprite('pirate', self))
 
     def calcPos(self, vector):
         return (
             vector[0]*self.u[0] + vector[1]*self.v[0] + self._window.get_width()/2,
             vector[0]*self.u[1] + vector[1]*self.v[1] + self._window.get_height()/2
         )
+
+    def calcVec(self, vector):
+        return (
+            vector[0]*self.u[0] + vector[1]*self.v[0],
+            vector[0]*self.u[1] + vector[1]*self.v[1]
+        )
+
+    def update(self):
+        p0 = self._scene.getPlayer(0)
+        p1 = self._scene.getPlayer(1)
+        dir0 = self.calcVec(p0.direction())
+        dir1 = self.calcVec(p1.direction())
+        pos0 = self.calcPos(p0.position())
+        pos1 = self.calcPos(p1.position())
+        
+#        self._players[0].update();
+#        self._players[1].update();
 
     def draw(self):
         self._window.fill(pygame.Color(255,255,255))    
