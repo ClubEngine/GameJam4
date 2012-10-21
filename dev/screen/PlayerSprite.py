63d84f2735cf9a2e0f00c0efb80dc12dc9b17377
Gilles <gilles@LinBox.(none)>
2012-10-21 16:03:25 +0200

    GJ4: bug

--- dev/screen/PlayerSprite.py
+++ dev/screen/PlayerSprite.py
@@ -6,20 +6,21 @@ il contient un tableau avec tout ses sprites
 """
 class PlayerSprite(pygame.sprite.Sprite):
     
-    def __init__(self, playerName, screen, pos = (0, 0), dir = (0, 0)):
+    def __init__(self, playerName, screen, pos = (0, 0), direction = (0, 0)):
         pygame.sprite.Sprite.__init__(self)
         self._spritesList = []
         self._screen = screen
         for i in range(0, 360, 10):
             sprite = pygame.image.load("assets/img/" + playerName + "-pos-rot/" + str(i).zfill(4) + ".png")
             self._spritesList.append(sprite)
-        self.update(pos, dir)
+        self.update(pos, direction)
         
-    def update(self, pos, dir):
-        # imageNb = angle / 10;
-        # self.image = self._spritesList[imageNb]
-        # self.rect = self.image.get_rect()
-        # pos = self._screen.calcPos((x,y))
-        # self.rect.x = pos[0]-75
-        # self.rect.y = pos[1]-85
+    def update(self, pos, direction):
+        #angle = dir
+        #imageNb = angle / 10;
+        #self.image = self._spritesList[imageNb]
+        #self.rect = self.image.get_rect()
+        #pos = self._screen.calcPos((x,y))
+        #self.rect.x = pos[0]-75
+        #self.rect.y = pos[1]-85
         pass
