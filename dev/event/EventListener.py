6d5e07a07127c122a15926694cec42bdc6c8d269
Pierre Turin <Pierre.Turin@ensimag.imag.fr>
2012-10-21 15:46:18 +0200

    newFrame() call added and elapsed time between frames passed in arguments

--- dev/event/EventListener.py
+++ dev/event/EventListener.py
@@ -4,21 +4,24 @@ from pygame.locals import *
 class EventListener:
     def __init__(self, scene):
         self._scene = scene
+        self._clock = pygame.time.Clock
         self._keysMap = dict({
-                pygame.K_z : [self._scene.moveForward, 0, 0],
-                pygame.K_s : [self._scene.moveBackward, 0, 0],
-                pygame.K_q : [self._scene.moveLeft, 0, 0],
-                pygame.K_d : [self._scene.moveRight, 0, 0],
-                pygame.K_e : [self._scene.jump, 0, 0],
-                pygame.K_SPACE : [self._scene.attack, 0, 0],
-                pygame.K_UP : [self._scene.moveForward, 1, 0],
-                pygame.K_DOWN : [self._scene.moveBackward, 1, 0],
-                pygame.K_LEFT : [self._scene.moveLeft, 1, 0],
-                pygame.K_RIGHT : [self._scene.moveRight, 1, 0],
-                pygame.K_RCTRL : [self._scene.jump, 1, 0],
-                pygame.K_KP0 : [self._scene.attack, 1, 0]})
+                pygame.K_z : [self._scene.moveForward, 0, False],
+                pygame.K_s : [self._scene.moveBackward, 0, False],
+                pygame.K_q : [self._scene.moveLeft, 0, False],
+                pygame.K_d : [self._scene.moveRight, 0, False],
+                pygame.K_e : [self._scene.jump, 0, False],
+                pygame.K_SPACE : [self._scene.attack, 0, False],
+                pygame.K_UP : [self._scene.moveForward, 1, False],
+                pygame.K_DOWN : [self._scene.moveBackward, 1, False],
+                pygame.K_LEFT : [self._scene.moveLeft, 1, False],
+                pygame.K_RIGHT : [self._scene.moveRight, 1, False],
+                pygame.K_RCTRL : [self._scene.jump, 1, False],
+                pygame.K_KP0 : [self._scene.attack, 1, False]})
 
     def listen(self):
+        self._clock.tick()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self._quit()
@@ -26,17 +29,19 @@ class EventListener:
                 if event.key == K_ESCAPE:
                     self._quit()
                 if event.key in self._keysMap:
-                    self._keysMap[event.key][2] = pygame.time.get_ticks()
+                    self._keysMap[event.key][2] = True
             elif event.type == pygame.KEYUP:
                 if event.key in self._keysMap:
-                    self._keysMap[event.key][2] = 0;
+                    self._keysMap[event.key][2] = False;
 
         for actionKey in self._keysMap:
-            time = self._keysMap[actionKey][2]
-            if time > 0:
+            isKeyDown = self._keysMap[actionKey][2]
+            if isKeyDown:
                 method = self._keysMap[actionKey][0]
                 playerIndex = self._keysMap[actionKey][1]
-                method(playerIndex)
+                method(playerIndex, self._clock.get_time())
+
+        self._scene.newFrame(self._clock.get_time())
 
     def _quit(self):
         pygame.quit()
