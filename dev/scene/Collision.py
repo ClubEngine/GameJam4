be90cdcc2d245260c7ce64fa7e947a75ef25ece8
Gilles <gilles@LinBox.(none)>
2012-10-21 17:43:29 +0200

    GJ4: collision

--- dev/scene/Collision.py
+++ dev/scene/Collision.py
@@ -11,12 +11,30 @@ class Collision:
     """
 
     def __init__(self, player0, player1):
-        self._player = [player0, player1]
+        self._players = [player0, player1]
         self.setCollisionProperties(0.5)
-        u = self.getDirection(0)
-        v = self.getDirection(1)
-        player0.setDirection(u)
-        player1.setDirection(v)
+        self._pos=[[0,0,0], [0,0,0]]
+        self._dir=[[0,0,0], [0,0,0]]
+        self._length = 0
+        #u = self.getDirection(0)
+        #v = self.getDirection(1)
+        #
+        #
+        self.actualize()
+
+    def actualize(self):
+        self._pos[0] = self._players[0].position()
+        self._pos[1] = self._players[1].position()
+        self._dir[0][0] = self._pos[0][0] - self._pos[1][0]
+        self._dir[0][1] = self._pos[0][1] - self._pos[1][1]
+        self._length = sqrt( self._dir[0][0] * self._dir[0][0] + self._dir[0][1] * self._dir[0][1] )
+        if (self._length != 0):
+            self._dir[0][0] /= self._length
+            self._dir[0][1] /= self._length
+        self._dir[1][0] = -self._dir[0][0]
+        self._dir[1][1] = -self._dir[0][1]
+        self._players[0].setDirection(self._dir[0])
+        self._players[1].setDirection(self._dir[1])
 
     """ affecte les proprietes des collisions """
     def setCollisionProperties(self, distanceCollisionPlayer):
@@ -24,21 +42,11 @@ class Collision:
 
     """ retourne la distance entre les deux persos """
     def getDistance(self):
-        v1 = self._player[(playerId + 1) % 2].position()
-        v2 = self._player[playerId].position()
-        u = [v1[0] - v2[0], v1[1] - v2[1]]
-        projectLength = sqrt(u[0] * u[0] + u[1] * u[1])
+        return self._length
         
     """ retourne la direction dans laquelle regarde le perso """
     def getDirection(self, playerId):
-        v1 = self._player[(playerId + 1) % 2].position()
-        v2 = self._player[playerId].position()
-        u = [v1[0] - v2[0], v1[1] - v2[1], 0]
-        projectLength = sqrt(u[0] * u[0] + u[1] * u[1])
-        if (projectLength != 0):
-            u[0] /= projectLength
-            u[1] /= projectLength
-        return u
+        return self._dir[playerId]
 
     """ deplace le perso playerId de distance metres
         playerId peut valoir 0 ou 1
@@ -46,31 +54,20 @@ class Collision:
         retourne true si les joueurs sont en contact apres les deplacement
     """
     def moveForward(self, playerId, distance):
-        collideOut = False
-        v1 = self._player[(playerId + 1) % 2].position()
-        v2 = self._player[playerId].position()
-        u = [v1[0] - v2[0], v1[1] - v2[1]]
-        projectLength = sqrt(u[0] * u[0] + u[1] * u[1])
-        if (projectLength != 0):
-            u[0] /= projectLength
-            u[1] /= projectLength
-
-        if (projectLength - self._distanceCollisionPlayers <= distance):
+        if (self._length - self._distanceCollisionPlayers <= distance):
             """ etat de collision """
             collideOut = True
-            deplacement = projectLength - self._distanceCollisionPlayers
+            deplacement = self._length - self._distanceCollisionPlayers
         else:
+            collideOut = False
             deplacement = distance
-
-        self._player[playerId].setPosition([v2[0] + u[0] * deplacement,
-                                            v2[1] + u[1] * deplacement,
-                                            v2[2]])
-
-        dir0 = self.getDirection(0)
-        dir1 = self.getDirection(1)
-        self._player[0].setDirection(dir0)
-        self._player[1].setDirection(dir1)
-
+            
+        v = self._dir[playerId]
+        pos = self._pos[playerId]
+        self._players[playerId].setPosition([pos[0] + v[0] * deplacement,
+                                             pos[1] + v[1] * deplacement,
+                                             pos[2]])
+        self.actualize()
         return collideOut
 
 
@@ -79,40 +76,29 @@ class Collision:
         distance peut etre positif ou negatif selon le sens
     """
     def moveSide(self, playerId, distance):
-        v1 = self._player[(playerId + 1) % 2].position()
-        v2 = self._player[playerId].position()
-        u = [v1[0] - v2[0], v1[1] - v2[1]]
-        projectLength = sqrt(u[0] * u[0] + u[1] * u[1])
-        if (projectLength != 0):
-            v = [u[1] / projectLength, -u[0] / projectLength]
-        else:
-            v = [0, 0]
-
-        self._player[playerId].setPosition([v2[0] + v[0] * distance,
-                                            v2[1] + v[1] * distance,
-                                            v2[2]])
-        dir0 = self.getDirection(0)
-        dir1 = self.getDirection(1)
-        self._player[0].setDirection(dir0)
-        self._player[1].setDirection(dir1)
-                
+        v = [ self._dir[playerId][1], -self._dir[playerId][0] ]
+        pos = self._pos[playerId]
+        self._players[playerId].setPosition([pos[0] + v[0] * distance,
+                                             pos[1] + v[1] * distance,
+                                             pos[2]])
+        self.actualize()
         
 
-#p0 = Player("Ninja")
-#p1 = Player("Pirate")
-#p1.setPosition([10,10,2])
-#c = Collision(p0, p1)
-#
-#c.moveForward(0, 30)
-#c.moveSide(0, 10)
-#
-#print p0.position()
-#print p1.position()
-#
-#print c.getDirection(0)
-#print c.getDirection(1)
-#
-#print p0.direction()
-#print p1.direction()
+p0 = Player("Ninja", [10,10,2])
+p1 = Player("Pirate", [-10,-10,2])
+
+c = Collision(p0, p1)
+
+c.moveForward(0, 30)
+c.moveSide(0, 10)
+
+print p0.position()
+print p1.position()
+
+print c.getDirection(0)
+print c.getDirection(1)
+
+print p0.direction()
+print p1.direction()
 
         
